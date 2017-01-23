84c12132379f6f0d679286f0fbb1195dd6eb6199
Amberlee Darold <adarold@asl-dhcp126.cr.usgs.gov>
2017-01-23 16:44:34 -0700

    spaces :-(

--- qc.py
+++ qc.py
@@ -69,45 +69,41 @@ def path_verify(path):
 client=fdsn.Client("IRIS")
 
 for station in stations:
-  filename="%s%d/%03d/%s.%s.seed" % (datadir,day.year,day.julday,network,station)
-  path_verify(filename)
-  if os.path.exists(filename):
-    st=read(filename)
-  else:
-    try:
-      st=client.get_waveforms(network,station,"*","*",day,day+secperday)
-      #Save data to selected location here
-      st.write(filename,format='MSEED',reclen=512)
-    except:
-      print("Unable to download data for %s %s" %(station,str(day)))
-  # Get all of our channel ids
-  ids=[]
-  for tr in st:
-   ids.append(tr.id)
-  ids=set(ids)
-  # Make sure our resp files are up to date
-  irisclient=iris.Client()
-  for ch in ids:
-   
-    n,s,loc,chan=ch.split('.')
-    try:
-		resp=irisclient.resp(network,station,location=loc,channel=chan,
-		  starttime=UTCDateTime('2004-001T00:00:00.0'),endtime=day+secperday,
-		  filename=respfilename(ch))
-		resp=irisclient.evalresp(network,station,loc,chan,
-		  filename="%s%s.png" % (qcfigs,ch),output='plot')
-	except:
-	  print("No response data for channel %s" % (ch))
-  data={}
-  for ch in ids:
-    print(respfilename(ch))
-    stch=st.select(id=ch) # Just take the data for a single channel
-    ppsd=PPSD(stch[0].stats,metadata=str(respfilename(ch)))
-    ppsd.add(stch)
-    figname="%s%d/%03d/%s.png" % (qcfigs,day.year,day.julday,ch)
-    path_verify(figname)
-    ppsd.plot(figname,cmap=pqlx)
-    data[ch]=ppsd.get_percentile(percentile=50)
-    
-  print(data)
+	filename="%s%d/%03d/%s.%s.seed" % (datadir,day.year,day.julday,network,station)
+	path_verify(filename)
+	if os.path.exists(filename):
+		st=read(filename)
+	else:
+		try:
+			st=client.get_waveforms(network,station,"*","*",day,day+secperday)
+			#Save data to selected location here
+			st.write(filename,format='MSEED',reclen=512)
+		except:
+			print("Unable to download data for %s %s" %(station,str(day)))
+	# Get all of our channel ids
+	ids=[]
+	for tr in st:
+	 ids.append(tr.id)
+	ids=set(ids)
+	# Make sure our resp files are up to date
+	irisclient=iris.Client()
+	for ch in ids:
+		n,s,loc,chan=ch.split('.')
+		try:
+			resp=irisclient.resp(network,station,location=loc,channel=chan,starttime=UTCDateTime('2004-001T00:00:00.0'),endtime=day+secperday,filename=respfilename(ch))
+			resp=irisclient.evalresp(network,station,loc,chan,filename="%s%s.png" % (qcfigs,ch),output='plot')
+		except:
+			print("No response data for channel %s" % (ch))
+	data={}
+	for ch in ids:
+		print(respfilename(ch))
+		stch=st.select(id=ch) # Just take the data for a single channel
+		ppsd=PPSD(stch[0].stats,metadata=str(respfilename(ch)))
+		ppsd.add(stch)
+		figname="%s%d/%03d/%s.png" % (qcfigs,day.year,day.julday,ch)
+		path_verify(figname)
+		ppsd.plot(figname,cmap=pqlx)
+		data[ch]=ppsd.get_percentile(percentile=50)
+	
+	print(data)
     
